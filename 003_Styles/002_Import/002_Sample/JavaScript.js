d08bab7ef8eaffb3f657756a99fa61705de5e516
mikeg <mike.gusiev@omnigon.com>
2016-12-15 01:18:48 +0200

    Webcomponents Student Programm

---
+++ 003_Styles/002_Import/002_Sample/JavaScript.js
@@ -0,0 +1,6 @@
+﻿window.addEventListener('load', function () {
+    // получение доступа к элементу link и чтение содержимого через свойства import 
+    var content = document.querySelector("#list-import").import;
+    var list = content.querySelector("ul");
+    document.body.appendChild(list);
+});
